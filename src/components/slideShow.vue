8e61a2f4483467405476a3e3c71966045aca61f1
zhanghuiyun <zhanghuiyun@zhanghuiyundeMacBook-Pro.local>
2017-07-06 23:07:03 +0800

    commit
    首页左侧 + 底部 + layout布局

---
+++ src/components/slideShow.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>hello world {{x}}</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		data (){
+			return {
+				x : 1
+			}
+		}
+	}
+</script>
+
+
+<style scoped>
+	
+</style>
